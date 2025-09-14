b740d1aada8dd12a7abb97b318c7683ebec4524b
Sturla-ingason <Sturladi@gmail.com>
2025-09-14 17:37:34 +0000

    cleaned upp further
    breytti þannig að allt er núna undir setup eða utils eftir því hvað það er fyrir utan frogger.js sem er main js filið

---
+++ JS/utils/globalVars.js
@@ -0,0 +1,25 @@
+var gl;
+var locPosition;
+var locColor;
+
+var uShiftLocY;
+var shiftAmountY = 0.0;
+var shiftAmountX = 0.0;
+var carShift = 0.0;
+
+var uThetaLoc;
+var theta = 0.0;
+
+//Buffers
+var bufferID_Player;
+
+var bufferID_topEndZone;
+var bufferID_bottomEndZone;
+
+var bufferID_road1;
+var bufferID_road2;
+var bufferID_road3;
+
+var bufferID_car1;
+var bufferID_car2;
+var bufferID_car3;
